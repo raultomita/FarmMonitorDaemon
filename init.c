1f8d5b9121c0db8d00d7ce5265e88e1418edb80f
Raul Tomita <raul.tomita@gmail.com>
2017-07-01 19:18:30 +0000

    Two buttons

--- init.c
+++ init.c
@@ -2,36 +2,41 @@
 #include <stdio.h>
 
 const int ledPinPwm = 18;
+const int ledPin2 = 27;
 const int ledPin = 17;
 const int btnPin = 23;
+const int btnPinRising = 24;
+
+void buttonPushed(void){
+	digitalWrite(ledPin, HIGH);
+	digitalWrite(ledPin2, LOW);
+}
+
+void buttonPushedRising(void){
+	digitalWrite(ledPin, LOW);
+	digitalWrite(ledPin2, HIGH);
+}
 
 int main(void)
 {
 	wiringPiSetupGpio();
 
 	pinMode(ledPin, OUTPUT);
+	pinMode(ledPin2, OUTPUT);
+
+	pinMode(btnPinRising, INPUT);
 	pinMode(btnPin, INPUT);
+
+	pullUpDnControl(btnPinRising, PUD_UP);
 	pullUpDnControl(btnPin, PUD_UP);
+
+	wiringPiISR(btnPin, INT_EDGE_RISING, &buttonPushed);
+	wiringPiISR(btnPinRising, INT_EDGE_RISING, &buttonPushedRising);
 	printf("Configuration complete \n");
 
-	digitalWrite(ledPin, HIGH);
-	printf("Write values\n");
-
-	while(1)
-	{
-		printf("Heartbeat\n");
-		delay(1000);
-
-		int readValue = digitalRead(btnPin);
-		printf("Value is:%d\n",readValue);
-		if(readValue){
-		   digitalWrite(ledPin, LOW);
-		}
-		else
-		{
-		   digitalWrite(ledPin, HIGH);
-		}
+	while(1){
+		printf("I'm alive\n");
+		delay(60000);
 	}
-
         return 0;
 }
