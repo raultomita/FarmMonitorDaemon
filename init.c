f9b384643866709919681e5b476546e382d8a759
Raul Tomita <raul.tomita@gmail.com>
2017-07-01 17:22:57 +0000

    start over

--- init.c
+++ init.c
@@ -1,14 +1,37 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
-const int ledPin = 23;
+const int ledPinPwm = 18;
+const int ledPin = 17;
+const int btnPin = 23;
 
 int main(void)
 {
 	wiringPiSetupGpio();
+
 	pinMode(ledPin, OUTPUT);
-	
-	digitalWrite(ledPin, LOW);
-        printf("Some test");
-	return 0;
-} 
+	pinMode(btnPin, INPUT);
+	pullUpDnControl(btnPin, PUD_UP);
+	printf("Configuration complete \n");
+
+	digitalWrite(ledPin, HIGH);
+	printf("Write values\n");
+
+	while(1)
+	{
+		printf("Heartbeat\n");
+		delay(1000);
+
+		int readValue = digitalRead(btnPin);
+		printf("Value is:%d\n",readValue);
+		if(readValue){
+		   digitalWrite(ledPin, LOW);
+		}
+		else
+		{
+		   digitalWrite(ledPin, HIGH);
+		}
+	}
+
+        return 0;
+}
