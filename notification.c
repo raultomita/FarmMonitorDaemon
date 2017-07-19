b784eebdc7acdbfddf29863671ab92c93d9870ab
Raul Tomita <raul.tomita@RTOMITA>
2017-07-19 14:21:54 +0300

    Do almost nothing

--- notification.c
+++ notification.c
@@ -1,8 +1,15 @@
-#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <pthread.h>
+#include <hiredis/hiredis.h>
+#include <hiredis/async.h>
+#include <hiredis/adapters/libevent.h>
 
 #include "pins.h"
-
+char * key4save;
+char * value4save;
 void * listenForNotifications(void * threadId)
 {
 	printf("[%ld] Listening fo notifications\n", (long)pthread_self());
@@ -10,6 +17,7 @@ void * listenForNotifications(void * threadId)
 	while(1)
 	{
 		pthread_cond_wait(&notificationCond, &notificationMutex);
+		
 		printf("[%ld] Notification Sent\n", (long)pthread_self());
 	}
 	pthread_mutex_unlock(&notificationMutex);
@@ -25,6 +33,10 @@ void initializeNotification(void)
 void saveAndNotify(char * key, char * data)
 {
 	pthread_mutex_lock(&notificationMutex);
+	
+	key4save = key;
+	value4save = data;
+	
 	pthread_cond_signal(&notificationCond);
 	pthread_mutex_unlock(&notificationMutex);
 }
