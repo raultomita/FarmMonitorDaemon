f2c040e8f3a898bd44b28488929a3be2bc31f489
Raul Tomita <raul.tomita@gmail.com>
2017-10-13 19:27:40 +0000

    resolve compole errors

--- src/logging.c
+++ src/logging.c
@@ -1,4 +1,8 @@
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 #include "main.h"
 
@@ -7,11 +11,15 @@ const char * infoLogFormat = "INFO  [%ld] %s\n";
 int logInfo (const char * format, ... )
 {
     char *message = (char *)malloc((strlen(infoLogFormat) + 12 + strlen(format)) * sizeof(char));
+    va_list args;
+    va_start(args, format);
     sprintf(message, infoLogFormat,  (long)pthread_self(), format);
-    printf(message, ...);
+    int result =  printf(message, args);
+    va_end(args);
+    return result;
 }
 
 int logError (const char * format, ... )
 {
-
+return 0;
 }
