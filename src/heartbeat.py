fb964fe4a82e1e9f67c75b93df36cb19d8a110ca
Raul T <raul.tomita@gmail.com>
2018-05-22 19:33:43 +0300

    add heartbeat handler

--- src/heartbeat.py
+++ src/heartbeat.py
@@ -1,13 +1,17 @@
 import baseThing
 import redisConn
+import logging
 from datetime import datetime
+
+logger = logging.getLogger(__name__)
 
 class Hearbeat(baseThing.Thing):
     def __init__(self):
         self.sentAt = datetime.min
 
     def handleCommand(self, command):
-        if command = "timer" and (datetime.now() - self.sentAt).total_seconds() > 420:
-            self.sentAt = datetime.now()
-            redisConn.enqueueCommand(f"heartbeat:{redisConn.hostname}")
+        if command == "timer" and (datetime.now() - self.sentAt).total_seconds() > 420:
+            self.sentAt = datetime.now()            
+            redisConn.enqueueCommand(f"heartbeat:{redisConn.hostName}")
+            logger.info(f"heartbeat:{redisConn.hostName} sent")
 
