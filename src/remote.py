b1ded0838ad53f55c7962244050ada381ea4ad9a
Raul Tomita <raul.tomita@gmail.com>
2018-12-23 17:48:04 +0200

    Add remote control

--- src/remote.py
+++ src/remote.py
@@ -1,24 +1,51 @@
-import asyncio
+import dispatcher
+import logging
+import threading
 from evdev import *
 
+logger = logging.getLogger(__name__)
 dev = InputDevice('/dev/input/event1')
-print(dev)
 
-async def helper(dev):
-     async for event in dev.async_read_loop():
-        if event.type == ecodes.EV_KEY:
-            print(categorize(event))
+logger.debug(dev)
 
-def getKey():
-    for event in dev.read_loop():
-        if event.type == ecodes.EV_KEY:
-            yield categorize(event)
+class RemoteControlThread(threading.Thread):
+    def __init__(self):
+        super(RemoteControlThread, self).__init__()
+        self.daemon = True
 
-#keygenerator = getKey()
+    def run(self):
+       logger.debug("Starting remote control thrread")
 
-# or you could consume the generator directly in a for loop
-#for c in getKey():
-#   print(c)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(helper(dev))
-print("test")
+       for event in dev.read_loop():
+           if event.type == ecodes.EV_KEY:
+               rcEvent = categorize(event)
+               logger.debug(rcEvent)
+
+               if rcEvent.keystate == rcEvent.key_down:
+                   if rcEvent.keycode == "KEY_POWER":
+                       dispatcher.enqueueCommand("switch23")
+                   elif rcEvent.keycode == "KEY_BACK":
+                       dispatcher.sendCommand("switch22")
+                   elif rcEvent.keycode == "KEY_NEXT":
+                       dispatcher.sendCommand("switch21")
+                   elif rcEvent.keycode == "KEY_PLAY":
+                       dispatcher.sendCommand("switch6:on")
+                       dispatcher.sendCommand("switch8:on")
+
+                       dispatcher.sendCommand("switch5:off")
+                       dispatcher.sendCommand("switch7:off")
+                       dispatcher.sendCommand("switch1:off")
+                       dispatcher.sendCommand("switch11:off")
+                       dispatcher.sendCommand("switch22:off")
+                       dispatcher.sendCommand("switch20:off")
+                       dispatcher.sendCommand("switch21:off")
+                   elif rcEvent.keycode == "KEY_STOP":
+                       dispatcher.sendCommand("switch6:off")
+                       dispatcher.sendCommand("switch8:off")
+                       dispatcher.sendCommand("switch5:off")
+                       dispatcher.sendCommand("switch7:off")
+                       dispatcher.sendCommand("switch1:off")
+                       dispatcher.sendCommand("switch11:off")
+                       dispatcher.sendCommand("switch22:off")
+                       dispatcher.sendCommand("switch21:off")
+                       dispatcher.sendCommand("switch20:off")
