09baaa5d02dda9e26c0f7d79d6bc39ebfe52b211
Raul Tomita <raul.tomita@gmail.com>
2020-01-05 19:11:55 +0200

    edit the input in remote

--- src/remote.py
+++ src/remote.py
@@ -4,7 +4,7 @@ import threading
 from evdev import *
 
 logger = logging.getLogger(__name__)
-dev = InputDevice('/dev/input/event5')
+dev = InputDevice('/dev/input/event1')
 print(dev)
 logger.debug(dev)
 
