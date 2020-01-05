cce8cc61346c389b3eb1c151857ae323e8acd909
Raul T <raul.tomita@gmail.com>
2020-01-06 01:47:39 +0200

    update install sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,8 @@ cd ..
 sudo apt-get -y install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
 tar -zxvf python3.6-release-build.tar.gz
 cd Python-3.6.5
+./configure
+make
 sudo make altinstall
 
 cd ..
